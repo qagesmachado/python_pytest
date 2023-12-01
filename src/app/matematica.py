45de189b406bde935cb8a534bfbb18e67cd3d1dd
Gustavo Eduardo Silva Machado <gu_silva_mg08@hotmail.com>
2023-12-01 13:45:09 -0300

    add new code

--- src/app/matematica.py
+++ src/app/matematica.py
@@ -2,3 +2,6 @@ class Matematica:
 
     def somar(v1, v2):
         return v1 + v2
+    
+    def multiplicar(v1, v2):
+        return v1 * v2
--- src/tests/test_matematica.py
+++ src/tests/test_matematica.py
@@ -3,3 +3,6 @@ from src.app.matematica import Matematica
 
 def test_somar():
     assert Matematica.somar(1,2) == 3
+
+def test_mult():
+    assert Matematica.multiplicar(2,3) == 7
